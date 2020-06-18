84bf97eb2cba0edac7002807248d056832b1ef4d
wjq990112 <1163585385@qq.com>
2020-06-18 10:41:13 +0800

    init: 项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState('Hello World!');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app"
+      // 用于获取 DOM 节点
+      data-testid="container"
+      onClick={() => {
+        setContent('Hello Jack!');
+      }}
+    >
+      {content}
     </div>
   );
 }
