3d8fd7680ca1721e5a52e45f21c6a5ab132f2904
wjq990112 <1163585385@qq.com>
2020-07-01 00:16:27 +0800

    feat: Add Test Case

--- src/__tests__/integration/App.tsx
+++ src/__tests__/integration/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import { render, fireEvent, RenderResult } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  act,
+  RenderResult,
+  waitForDomChange
+} from '@testing-library/react';
 import App from '../../App';
+import axios from 'axios';
+
+jest.mock('axios');
 
 let wrapper: RenderResult;
 
 // 运行每一个测试用例前先渲染组件
-beforeEach(() => {
-  wrapper = render(<App />);
+beforeEach(async () => {
+  axios.get.mockResolvedValue({
+    data: {
+      code: 200,
+      data: [
+        {
+          status: 'div',
+          value: '学习 Jest'
+        },
+        {
+          status: 'div',
+          value: '学习 Enzyme'
+        },
+        {
+          status: 'div',
+          value: '学习 Testing-Library'
+        }
+      ],
+      message: 'success'
+    }
+  });
+  await act(async () => {
+    wrapper = render(<App />);
+  });
 });
 
 // 运行后重置
@@ -15,5 +46,14 @@ afterAll(() => {
 });
 
 describe('App 组件', () => {
-  test('测试组件初始化后的状态', () => {});
+  test('测试组件初始化后的状态', () => {
+    const count = wrapper.queryByTestId('count');
+    // 计数器存在且数值为 3
+    expect(count).not.toBeNull();
+    expect(count.textContent).toEqual('3');
+
+    const list = wrapper.queryAllByTestId('list-item');
+    // 列表项不为空且长度为 3
+    expect(list).toHaveLength(3);
+  });
 });
